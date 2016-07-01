7fed98a7fe4b169f1dc9a75c6443a4d230a1a16a
Philipp Kraus <philipp.kraus@flashpixx.de>
2016-07-01 14:59:17 +0200

    add github params

--- deploy.bash
+++ deploy.bash
@@ -8,6 +8,7 @@ git branch -D master
 git checkout --orphan master
 rm -Rf *
 mv /tmp/public/* .
+echo '{ "name" : "LightJason.GitHub.io", "tagline" : "", "body" : "" }' > params.json
 git add --all .
 git commit -m "current documentation"
 git push origin master
