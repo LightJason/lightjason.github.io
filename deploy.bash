22095042a7916b8d931d30cac85e3cf35d9d02ae
Philipp Kraus <philipp.kraus@flashpixx.de>
2016-07-01 15:03:40 +0200

    fix deploy

--- deploy.bash
+++ deploy.bash
@@ -7,8 +7,10 @@ git push origin :master
 git branch -D master
 git checkout --orphan master
 rm -Rf *
+echo -n "*.*\n!.gitignore\n!*.html\n!*.xml\n!*.css\n!*.js\n!*.json"
 mv /tmp/public/* .
 echo '{ "name" : "LightJason.GitHub.io", "tagline" : "", "body" : "" }' > params.json
 git add --all .
 git commit -m "current documentation"
 git push origin master
+git checkout developing
