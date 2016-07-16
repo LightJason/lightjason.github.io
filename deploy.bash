cb573fd8884806076637e609c2fe63296331b0e1
Philipp Kraus <philipp.kraus@flashpixx.de>
2016-07-16 20:25:51 +0200

    add prune

--- deploy.bash
+++ deploy.bash
@@ -4,6 +4,7 @@ hugo
 rm -Rf /tmp/public
 mv -f public /tmp
 git branch -D master
+git gc --prune=now
 git checkout --orphan master
 rm -Rf *
 echo -n "*.*\n!.gitignore\n!*.html\n!*.xml\n!*.css\n!*.js\n!*.json\n!*.bib\n!*.md" > .gitignore
