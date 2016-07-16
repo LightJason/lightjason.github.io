7ee7c16592e1f4b8f9a8e048aae2ce11f05c6b79
Philipp Kraus <philipp.kraus@flashpixx.de>
2016-07-16 18:50:01 +0200

    change deploy

--- deploy.bash
+++ deploy.bash
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 hugo
-git push origin :master
 rm -Rf /tmp/public
-mv public /tmp
-git push origin :master
+mv -f public /tmp
 git branch -D master
 git checkout --orphan master
 rm -Rf *
-echo -n "*.*\n!.gitignore\n!*.html\n!*.xml\n!*.css\n!*.js\n!*.json\n!*.bib" > .gitignore
-mv /tmp/public/* .
-echo '{ "name" : "LightJason.GitHub.io", "tagline" : "", "body" : "" }' > params.json
+echo -n "*.*\n!.gitignore\n!*.html\n!*.xml\n!*.css\n!*.js\n!*.json\n!*.bib\n!*.md" > .gitignore
+mv -f /tmp/public/* .
+echo '# LightJason' > readme.md
 git add --all .
 git commit -m "current documentation"
+#git push origin :master
 git push origin master
 git checkout developing
