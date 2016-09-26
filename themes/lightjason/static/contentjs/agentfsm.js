d5f13f1de6d8bbd1c25d85bd8b6b8342a534ce24
Philipp Kraus <philipp.kraus@flashpixx.de>
2016-09-26 22:33:37 +0200

    change animation definition

--- themes/lightjason/static/contentjs/agentfsm.js
+++ themes/lightjason/static/contentjs/agentfsm.js
@@ -1,6 +1,17 @@
 // animates the svg image of the agent-finite-state-machine
 $(document).ready(function() {
 	$("#animate-agentfsm").click( function() {
-		$("#init, #main, #first, #second").css("animation-play-state", "running");
+		$("#init").css("animation-name", "colorchange");
+		$("#init").css("animation-duration", "1.5s");
+
+		$("#main").css("animation-name", "colorchange");
+		$("#main").css("animation-duration", "3s");
+		$("#main").css("animation-delay", "2.75s");
+		$("#main").css("animation-iteration-count", "infinite");
+
+		$("#first, #second").css("animation-name", "colorchange");
+		$("#first, #second").css("animation-duration", "3s");
+		$("#first, #second").css("animation-delay", "4.25s");
+		$("#first, #second").css("animation-iteration-count", "infinite");
 	} );
 } );
