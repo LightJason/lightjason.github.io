2a47e09d386ee3d36626ecba5767578a239e11b6
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-01-10 14:00:33 +0100

    remove minify

--- data/minifyhtml.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh -e
-
-MINIFY=$(where html-minifier)
-TMP=$(mktemp)
-
-$MINIFY --case-sensitive --collapse-boolean-attributes --collapse-whitespace --decode-entities --html5 --minify-css --minify-js --quote-character \" --remove-attribute-quotes --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype -o $TMP $1
-mv -f $TMP $1
