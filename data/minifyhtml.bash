937f9eb8bee17b630ee6a20ec89a19fef54510db
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-01-10 12:35:25 +0100

    fix path minify script

--- data/minifyhtml.bash
+++ data/minifyhtml.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 TMP=$(mktemp)
-/usr/local/bin/html-minifier --html5 --remove-redundant-attributes --remove-tag-whitespace --case-sensitive -o $TMP $1
+html-minifier --html5 --remove-redundant-attributes --remove-tag-whitespace --case-sensitive -o $TMP $1
 mv -f $TMP $1
 
