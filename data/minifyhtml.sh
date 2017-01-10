6b57c226ce63769d153f4ec547c11b090b393cae
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-01-10 22:06:12 +0100

    fix command

--- data/minifyhtml.sh
+++ data/minifyhtml.sh
@@ -1,6 +1,7 @@
-#!/bin/zsh -e
+#!/bin/zsh
 
-MINIFY=$(where html-minifier)
+MINIFYCMD=$(where html-minifier)
+MINIFY=${MINIFYCMD%%\n}
 TMP=$(mktemp)
 
 $MINIFY --case-sensitive --collapse-boolean-attributes --collapse-whitespace --decode-entities --html5 --minify-css --minify-js --quote-character \" --remove-attribute-quotes --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype -o $TMP $1
