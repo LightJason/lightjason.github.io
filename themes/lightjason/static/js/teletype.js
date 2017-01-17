2f5f89d0953d6428a95cd12992498bd27b2021f8
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-01-17 20:49:48 +0100

    refactor

--- themes/lightjason/static/js/teletype.js
+++ themes/lightjason/static/js/teletype.js
@@ -28,180 +28,204 @@
 				loop: 0
 			};
 		
-		// default plugin settings
-		var defaults = {
-			text: [ 'one', 'two', 'three' ],
-			result: [],
-			automaticstart: true,
-			classresult: "teletype-result",
-			classcursor: "teletype-cursor",
-			classprefix: "teletype-prefix",
-			classtext: "teletype-text",
-			classmain: "teletype",
-			typeDelay: 100,
-			backDelay: 50,
-			blinkSpeed: 1000,
-			delay: 2000,
-			cursor: '|',
-			preserve: false,
-			prefix: '',
-			loop: 0,
-			humanise: true,
-			smoothBlink: true,
-			callbackNext: null,
-			callbackType: null,
-			callbackFinished: null
-		}
-
-		// sets instance values into object
-		object.settings = jQuery.extend( {}, defaults, options );
-
-
-
-		var next = function() {
-			current.index++;
-			if ( current.index >= settings.text.length ) {
-				current.index = 0;
-				current.loop++;
-				if ( settings.loop !== false && ( settings.loop == current.loop ) )
-					return false;
+			// default plugin settings
+			var defaults = {
+				text: [ 'one', 'two', 'three' ],
+				result: [],
+				automaticstart: true,
+				classresult: "teletype-result",
+				classcursor: "teletype-cursor",
+				classprefix: "teletype-prefix",
+				classtext: "teletype-text",
+				classmain: "teletype",
+				typeDelay: 100,
+				backDelay: 50,
+				blinkSpeed: 1000,
+				delay: 2000,
+				cursor: '|',
+				preserve: false,
+				prefix: '',
+				loop: 0,
+				humanise: true,
+				smoothBlink: true,
+				callbackNext: null,
+				callbackType: null,
+				callbackFinished: null
 			}
 
-			current.position = 0;
-			setCurrentString();
-			if ( typeof( settings.callbackNext ) == 'function' )
-				settings.callbackNext( current, object );
-
-			return true;
-		};
+			// sets instance values into object
+			object.settings = jQuery.extend( {}, defaults, options );
 
 
-		var type = function() {
-			if ( ( settings.prefix && current.position === 0 ) && ( current.loop === 0 && current.index === 0 ) )
-					jQuery( '<span />' ).addClass( settings.classprefix ).html( settings.prefix ).prependTo( self );
 
-			var letters = current.string.split( '' ),
-				letter = letters[current.position],
-				start = current.position + 1;
+			// ---- private methods -------------------------------------------------------------------------------------------------
 
-			if ( letter == '^' || letter == '~' ) {
-				var end = current.string.substr( start ).search( /[^0-9]/ );
-				if ( end == -1 )
-					end = current.string.length;
+			var next = function() {
+				current.index++;
+				if ( current.index >= settings.text.length ) {
+					current.index = 0;
+					current.loop++;
+					if ( settings.loop !== false && ( settings.loop == current.loop ) )
+						return false;
+				}
+
+				current.position = 0;
+				setCurrentString();
+				if ( typeof( settings.callbackNext ) == 'function' )
+					settings.callbackNext( current, object );
+
+				return true;
+			};
+
+
+			var type = function() {
+				if ( ( settings.prefix && current.position === 0 ) && ( current.loop === 0 && current.index === 0 ) )
+						jQuery( '<span />' ).addClass( settings.classprefix ).html( settings.prefix ).prependTo( self );
+
+				var letters = current.string.split( '' ),
+					letter = letters[current.position],
+					start = current.position + 1;
+
+				if ( letter == '^' || letter == '~' ) {
+					var end = current.string.substr( start ).search( /[^0-9]/ );
+					if ( end == -1 )
+						end = current.string.length;
+					
+					var value = current.string.substr( start, end );
+					if ( jQuery.isNumeric( value ) ) {
+						current.string = current.string.replace( letter + value, '' );
+
+						if ( letter == '^' )
+							window.setTimeout( function() { window.setTimeout( type, delay( settings.typeDelay ) ); }, value );
+
+						else {
+							var index = current.position - value;
+							current.string = current.string.substr( 0, index - 1 ) + current.string.substr( current.position - 1 );
+							window.setTimeout( function() { backspace( Math.max( index, 0 ) ); }, delay( settings.backDelay ) );
+						}
+
+						return;
+					}
+
+				} else 
+					if ( letter == '\\' ) {
+						var nextChar = current.string.substr( start, 1 );
+						if ( nextChar == 'n' ) {
+							current.position++;
+							letter = '<br />';
+						}
+					}
+
+				if ( ( output ) && ( letter ) )
+					output.html( output.html() + letter );
 				
-				var value = current.string.substr( start, end );
-				if ( jQuery.isNumeric( value ) ) {
-					current.string = current.string.replace( letter + value, '' );
-
-					if ( letter == '^' )
-						window.setTimeout( function() { window.setTimeout( type, delay( settings.typeDelay ) ); }, value );
-
-					else {
-						var index = current.position - value;
-						current.string = current.string.substr( 0, index - 1 ) + current.string.substr( current.position - 1 );
-						window.setTimeout( function() { backspace( Math.max( index, 0 ) ); }, delay( settings.backDelay ) );
-					}
-
-					return;
-				}
-
-			} else 
-				if ( letter == '\\' ) {
-					var nextChar = current.string.substr( start, 1 );
-					if ( nextChar == 'n' ) {
-						current.position++;
-						letter = '<br />';
-					}
-				}
-
-			if ( ( output ) && ( letter ) )
-				output.html( output.html() + letter );
-			
-			current.position++;
-			if ( current.position < current.string.length )
-				window.setTimeout( type, delay( settings.typeDelay ) );
-			else 
-				if ( settings.preserve === false )
-					window.setTimeout( function() { window.setTimeout( backspace, delay( settings.backDelay ) ); }, settings.delay );
-			else {
-				output.html( output.html() + current.result + '<span class="' + settings.classprefix+ '">' + settings.prefix + '</span>' );
-				if ( next() )
-					window.setTimeout( function() { window.setTimeout( type, delay( settings.typeDelay ) ); }, settings.delay );
+				current.position++;
+				if ( current.position < current.string.length )
+					window.setTimeout( type, delay( settings.typeDelay ) );
 				else 
-					if ( typeof( settings.callbackFinished ) == 'function' )
-						settings.callbackFinished( object );
+					if ( settings.preserve === false )
+						window.setTimeout( function() { window.setTimeout( backspace, delay( settings.backDelay ) ); }, settings.delay );
+				else {
+					output.html( output.html() + current.result + '<span class="' + settings.classprefix+ '">' + settings.prefix + '</span>' );
+					if ( next() )
+						window.setTimeout( function() { window.setTimeout( type, delay( settings.typeDelay ) ); }, settings.delay );
+					else 
+						if ( typeof( settings.callbackFinished ) == 'function' )
+							settings.callbackFinished( object );
+				}
+
+				if ( typeof( settings.callbackType ) == 'function' )
+					settings.callbackType( letter, current, object );
+			
+			};
+
+
+			var backspace = function( stop ) {
+				if ( !stop )
+					stop = 0;
+				
+				if ( current.position > stop ) {
+					if (output)
+						output.html( output.html().slice( 0, -1 ) );
+					window.setTimeout( function() { backspace( stop ); }, delay( settings.backDelay ) );
+					current.position--;
+				
+				} else {
+					if ( ( stop === 0 ) && ( next() === false ) )
+						return;
+				
+					window.setTimeout( type, delay( settings.typeDelay ) );
+				}
+			};
+
+
+			var delay = function( speed ) { return settings.humanise ? parseInt( speed ) + Math.floor( Math.random() * 200 ) : parseInt( speed ); };
+
+
+			var setCurrentString = function() {
+				current.string = settings.text[current.index].replace(/\n/g, "\\n");
+				current.result = (settings.result.length == settings.text.length) && (!!settings.result[current.index]) ? '<p class="' + settings.classresult + '">' + settings.result[current.index] + "</p>" : "";
 			}
 
-			if ( typeof( settings.callbackType ) == 'function' )
-				settings.callbackType( letter, current, object );
-		
-		};
 
 
-		var backspace = function( stop ) {
-			if ( !stop )
-				stop = 0;
+			// ---- public methods --------------------------------------------------------------------------------------------------
+
+			object.setCursor = function( cursor ) {
+				jQuery('.'+settings.classcursor, self).text( cursor );
+			};
 			
-			if ( current.position > stop ) {
-				if (output)
-					output.html( output.html().slice( 0, -1 ) );
-				window.setTimeout( function() { backspace( stop ); }, delay( settings.backDelay ) );
-				current.position--;
 			
-			} else {
-				if ( ( stop === 0 ) && ( next() === false ) )
+			object.reset = function() {
+				if (settings.loop === 0)
 					return;
-			
-				window.setTimeout( type, delay( settings.typeDelay ) );
+			};
+
+
+			object.start = function() {
+				//if (settings.automaticstart)
+				//	return;
 			}
-		};
 
 
-		var delay = function( speed ) { return settings.humanise ? parseInt( speed ) + Math.floor( Math.random() * 200 ) : parseInt( speed ); };
+			object.init = function() {
 
+			}
 
-		var setCurrentString = function() {
-			current.string = settings.text[current.index].replace(/\n/g, "\\n");
-			current.result = (settings.result.length == settings.text.length) && (!!settings.result[current.index]) ? '<p class="' + settings.classresult + '">' + settings.result[current.index] + "</p>" : "";
+			object.init();
 		}
 
-
-		this.setCursor = function( cursor ) {
-			jQuery('.'+settings.classcursor, self).text( cursor );
-		};
-        
-		
-		this.reset = function() {
-			if (settings.loop === 0)
-				return;
-        };
+		// ---- jQuery initialization -------------------------------------------------------------------------------------------
 
 
-		this.start = function() {
-			//if (settings.automaticstart)
-			//	return;
-		}
 
+		jQuery.fn.teletype = function( options ) {
 
-		return this.each( function() {
-			// http://stefangabos.ro/jquery/jquery-plugin-boilerplate-revisited/
+			return this.each( function() {
+				// http://stefangabos.ro/jquery/jquery-plugin-boilerplate-revisited/
 
-			setCurrentString();
-			self.addClass( settings.classmain ).empty();
-			output = jQuery( '<span />' ).addClass( settings.classtext ).appendTo( self );
-			if ( settings.cursor ) {
-				var cursor = $( '<span />' ).addClass( settings.classcursor ).appendTo( self );
-				object.setCursor( settings.cursor );
-				setInterval ( function() {
-                	if ( settings.smoothBlink )
-				    	cursor.animate( { opacity: 0 } ).animate( { opacity: 1 } );
-			      	else
-                    	cursor.delay(500).fadeTo(0,0).delay(500).fadeTo(0,1);
-				}, settings.blinkSpeed );
-			}
-			if (settings.automaticstart)
-				type();	
-		} );
-	};
+				if (undefined == jQuery(this).data('teletype'))
+					return jQuery(this).data( 'teletype', new jQuery.teletype(this, options) );
+
+				/*
+				setCurrentString();
+				self.addClass( settings.classmain ).empty();
+				output = jQuery( '<span />' ).addClass( settings.classtext ).appendTo( self );
+				if ( settings.cursor ) {
+					var cursor = $( '<span />' ).addClass( settings.classcursor ).appendTo( self );
+					object.setCursor( settings.cursor );
+					setInterval ( function() {
+						if ( settings.smoothBlink )
+							cursor.animate( { opacity: 0 } ).animate( { opacity: 1 } );
+						else
+							cursor.delay(500).fadeTo(0,0).delay(500).fadeTo(0,1);
+					}, settings.blinkSpeed );
+				}
+				if (settings.automaticstart)
+					type();	
+				*/
+
+			} );
+	}
 
 })(jQuery);
