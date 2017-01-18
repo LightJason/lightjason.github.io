3a1e1f5664248d8c772bfaa480a846799fff17bc
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-01-18 20:15:58 +0100

    remove minify option

--- data/minifyhtml.sh
+++ data/minifyhtml.sh
@@ -3,5 +3,5 @@
 MINIFY=$(which html-minifier)
 
 TMP=$(mktemp)
-$MINIFY --case-sensitive --collapse-boolean-attributes --collapse-whitespace --decode-entities --html5 --minify-css --minify-js --quote-character \" --remove-attribute-quotes --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype -o $TMP $1
+$MINIFY --case-sensitive --collapse-boolean-attributes --collapse-whitespace --decode-entities --html5 --minify-css --minify-js --quote-character \" --remove-attribute-quotes --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype -o $TMP $1
 mv -f $TMP $1
