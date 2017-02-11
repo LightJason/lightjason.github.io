4bcdd70af4a692aa2e71f9a8e8b609cc874471e5
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-02-11 02:38:45 +0100

    add literal animation

--- static/js/agentfsm.js
+++ static/js/agentfsm.js
@@ -1,17 +1,21 @@
-// animates the svg image of the agent-finite-state-machine
-$(document).ready(function() {
-	$("#animate-agentfsm").click( function() {
-		$("#init").css("animation-name", "colorchange");
-		$("#init").css("animation-duration", "1.5s");
+"use strict";
 
-		$("#main").css("animation-name", "colorchange");
-		$("#main").css("animation-duration", "3s");
-		$("#main").css("animation-delay", "2.75s");
-		$("#main").css("animation-iteration-count", "infinite");
+jQuery(function() {
 
-		$("#first, #second").css("animation-name", "colorchange");
-		$("#first, #second").css("animation-duration", "3s");
-		$("#first, #second").css("animation-delay", "4.25s");
-		$("#first, #second").css("animation-iteration-count", "infinite");
+	// animates the svg image of the agent-finite-state-machine
+	jQuery("#animate-agentfsm").click(function() {
+		jQuery("#init").css("animation-name", "colorchange");
+		jQuery("#init").css("animation-duration", "1.5s");
+
+		jQuery("#main").css("animation-name", "colorchange");
+		jQuery("#main").css("animation-duration", "3s");
+		jQuery("#main").css("animation-delay", "2.75s");
+		jQuery("#main").css("animation-iteration-count", "infinite");
+
+		jQuery("#first, #second").css("animation-name", "colorchange");
+		jQuery("#first, #second").css("animation-duration", "3s");
+		jQuery("#first, #second").css("animation-delay", "4.25s");
+		jQuery("#first, #second").css("animation-iteration-count", "infinite");
 	} );
+
 } );
---
+++ static/js/literal.js
@@ -0,0 +1,26 @@
+"use strict";
+
+jQuery(function() {
+
+	// defines hover effects for literal
+	jQuery("#animate-literal-annotation").hover( 
+		function() { jQuery(".svg-literal-annotation").attr("fill", "red"); },
+		function() { jQuery(".svg-literal-annotation").attr("fill", ""); } 
+	);
+
+	jQuery("#animate-literal-values").hover( 
+		function() { jQuery(".svg-literal-values").attr("fill", "red"); },
+		function() { jQuery(".svg-literal-values").attr("fill", ""); } 
+	);
+
+	jQuery("#animate-literal-functor").hover( 
+		function() { jQuery(".svg-literal-functor").attr("fill", "red"); },
+		function() { jQuery(".svg-literal-functor").attr("fill", ""); } 
+	);
+
+	jQuery("#animate-literal-negation").hover( 
+		function() { jQuery(".svg-literal-negation").attr("fill", "red"); },
+		function() { jQuery(".svg-literal-negation").attr("fill", ""); } 
+	);	
+	
+} );
