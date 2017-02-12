353f81df988c96043c75dee7297a2fe6e6a9c769
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-02-12 21:26:14 +0100

    add agent cycle animation

---
+++ static/js/agentcycle.js
@@ -0,0 +1,29 @@
+"use strict";
+
+jQuery(function() {
+
+	// defines hover effects for literal
+	[ "literal-annotation", "literal-values", "literal-functor", "literal-negation" ].forEach( function(i) {
+		jQuery("#animate-"+i).hover( 
+			function() { jQuery(".svg-"+i).attr("fill", "red"); },
+			function() { jQuery(".svg-"+i).attr("fill", ""); } 
+		);
+	});
+
+	jQuery("#animate-literal-raw").hover( 
+			function() { jQuery(".svg-literal-raw").attr("fill", "blue"); },
+			function() { jQuery(".svg-literal-raw").attr("fill", ""); } 
+		);
+
+
+	// defines hover effects for both agent-cycles
+	[ ".svg-agentcycle-checkcontext", ".svg-agentcycle-unify", 
+	 ".svg-agentcycle-execution", ".svg-agentcycle-beliefbase", ".svg-agentcycle-plans"
+	].forEach( function(i) {
+		jQuery(i).hover( 
+			function() { jQuery(i).attr("fill", "#0f0"); },
+			function() { jQuery(i).attr("fill", "#dfd"); } 
+		);
+	});
+	
+} );
