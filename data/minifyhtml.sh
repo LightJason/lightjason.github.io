aff2f2b237aae852581021aeb31b14230a920645
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-02-24 00:19:36 +0100

    fix minify

--- data/minifyhtml.sh
+++ data/minifyhtml.sh
@@ -4,4 +4,4 @@ MINIFY=$(which html-minifier)
 
 TMP=$(mktemp)
 $MINIFY --case-sensitive --collapse-boolean-attributes --collapse-whitespace --decode-entities --html5 --minify-css --minify-js --remove-comments --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --trim-custom-fragments --use-short-doctype -o $TMP $1
-#mv -f $TMP $1
+mv -f $TMP $1
