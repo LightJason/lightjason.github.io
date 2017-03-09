a5490a52f39ebb261c5c059394f65ddb98d325d9
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-03-09 13:46:52 +0100

    update runtime

--- src/main/java/PACKAGE/CRuntime.java
+++ src/main/java/PACKAGE/CRuntime.java
@@ -1,13 +1,20 @@
 package {{ package }};
 
 import org.lightjason.agentspeak.agent.IAgent;
+import org.lightjason.agentspeak.action.IAction;
+import org.lightjason.agentspeak.common.CCommon;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
 
 import java.io.FileInputStream;
 import java.util.Collections;
 import java.util.Set;
 import java.util.logging.LogManager;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 
 /**
@@ -27,7 +34,7 @@ final class CRuntime
         {{ disablelogger }}LogManager.getLogManager().reset();
 
         // action initialize
-        ACTION = {{ actions }};
+        ACTIONS = {{ actions }}.collect( Collectors.toSet() );
     }
 
 
@@ -47,6 +54,8 @@ final class CRuntime
     public static void main( final String[] p_args )
     {
         final CommandLine l_cli = CRuntime.parsearguments( p_args );
+        if (l_cli == null)
+            System.exit( -1 );
     }
 
 
@@ -59,7 +68,10 @@ final class CRuntime
         final Options l_clioptions = new Options();
 
         l_clioptions.addOption( "help", false, "shows this information" );
+        l_clioptions.addOption( "sequential", false, "agents run in sequential order (default: parallel" );
         l_clioptions.addOption( "asl", true, "comma-sparated list of ASL files" );
+        l_clioptions.addOption( "agents", true, "comma-sparated list of generating agent numbers (equal to asl-flag)" );
+        l_clioptions.addOption( "steps", true, "number of simulation steps (default: integer maximum)" );
 
 
         // --- process CLI arguments and initialize configuration ----------------------------------------------------------------------------------------------
@@ -71,8 +83,7 @@ final class CRuntime
         catch ( final Exception l_exception )
         {
             System.err.println( "command-line arguments parsing error" );
-            System.exit( -1 );
-            return;
+            return null;
         }
 
         if ( l_cli.hasOption( "help" ) )
