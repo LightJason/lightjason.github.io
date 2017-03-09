29d7227c7d1e23056b15916d72a717d984729b0f
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-03-09 15:00:26 +0100

    add template documentation, update runtime

--- src/main/java/PACKAGE/CRuntime.java
+++ src/main/java/PACKAGE/CRuntime.java
@@ -10,11 +10,14 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 
 import java.io.FileInputStream;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.LogManager;
 import java.util.stream.Collectors;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 /**
@@ -26,6 +29,10 @@ final class CRuntime
      * global set with all possible agent actions
      */
     private static final Set<IAction> ACTIONS;
+    /**
+     * runtime agent collection
+     */
+    private static final Map<String, IAgent<?>> AGENTS = new ConcurrentHashMap<>();
 
 
     static
@@ -33,8 +40,8 @@ final class CRuntime
         // logger
         {{ disablelogger }}LogManager.getLogManager().reset();
 
-        // action initialize
-        ACTIONS = {{ actions }}.collect( Collectors.toSet() );
+        // action initialize and storing within an unmodifyable structure
+        ACTIONS = Collections.unmodifiableSet( {{ actions }}.collect( Collectors.toSet() ) );
     }
 
 
@@ -42,8 +49,7 @@ final class CRuntime
      * private constructor to avoid any instantiation
      */
     private CRuntime()
-    {
-    }
+    {}
 
 
     /**
@@ -56,6 +62,63 @@ final class CRuntime
         final CommandLine l_cli = CRuntime.parsearguments( p_args );
         if (l_cli == null)
             System.exit( -1 );
+
+
+        // generate agents
+        Arrays.stream( l_cli.getOptionValue( "asl", "" ).split( ",") )
+              .map( String::trim )
+              .forEach( System.out::println );
+
+        Arrays.stream( l_cli.getOptionValue( "agents", "" ).split(",") )
+              .mapToInt( Integer::parseInt )
+              .forEach( System.out::println );
+
+
+        // execute simulation
+        CRuntime.execute(
+            l_cli.hasOption( "steps" ) ? Integer.parseInt( l_cli.getOptionValue( "steps" ) ) : Integer.MAX_VALUE,
+            l_cli.hasOption( "sequential" )
+        );       
+    }
+
+
+        /**
+     * executes the simulation
+     *
+     * @param p_steps number of simulation steps
+     * @param p_parallel run agents in parallel
+     */
+    private static void execute( final int p_steps, final boolean p_parallel )
+    {
+        if (p_parallel)
+            IntStream.range( 0, p_steps )
+                 .forEach( i -> AGENTS.values()
+                                      .parallelStream()
+                                      .forEach( j -> {
+                                          try
+                                          {
+                                             j.call();
+                                          }
+                                          catch ( final Exception l_exception )
+                                          {
+                                             {{ disablelogger }}l_exception.printStackTrace();
+                                          }
+                                      } )
+                 );
+        else
+            IntStream.range( 0, p_steps )
+                 .forEach( i -> AGENTS.values()
+                                      .forEach( j -> {
+                                          try
+                                          {
+                                              j.call();
+                                          }
+                                          catch ( final Exception l_exception )
+                                          {
+                                              {{ disablelogger }}l_exception.printStackTrace();
+                                          }
+                                      } )
+                 );
     }
 
 
@@ -68,7 +131,7 @@ final class CRuntime
         final Options l_clioptions = new Options();
 
         l_clioptions.addOption( "help", false, "shows this information" );
-        l_clioptions.addOption( "sequential", false, "agents run in sequential order (default: parallel" );
+        l_clioptions.addOption( "sequential", false, "agents run in sequential order (default: parallel)" );
         l_clioptions.addOption( "asl", true, "comma-sparated list of ASL files" );
         l_clioptions.addOption( "agents", true, "comma-sparated list of generating agent numbers (equal to asl-flag)" );
         l_clioptions.addOption( "steps", true, "number of simulation steps (default: integer maximum)" );
