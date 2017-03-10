bf430abd474c994edf078d1d84dcba466b8e39ce
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-03-10 20:18:52 +0100

    add generator template

---
+++ src/main/java/PACKAGE/CAGENTGenerator.java
@@ -0,0 +1,29 @@
+package {{ package }};
+
+import org.lightjason.agentspeak.generator.IBaseAgentGenerator;
+import org.lightjason.agentspeak.language.score.IAggregation;
+
+import java.io.InputStream;
+
+final class C{{ agent }}Generator extends IBaseAgentGenerator<C{{ agent }}Agent>
+{
+
+    /**
+     * constructor of the generator
+     *
+     * @param p_stream asl stream
+     * @throws Exception on any error
+     */
+    MyAgentGenerator( final InputStream p_stream ) throws Exception
+    {
+        super( p_stream, IAggregation.EMPTY );
+
+    }
+
+    @Override
+    public final C{{ agent }}Agent generatesingle( final Object... p_data )
+    {
+        return new C{{ agent }}Agent( m_configuration );
+    }
+
+}
