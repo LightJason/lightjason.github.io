d7af922ce25febcf02d6eba61396c8630b9e2484
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-03-16 14:44:22 +0100

    update action definition

--- src/main/java/PACKAGE/actions/CACTIONNAMEAction.java
+++ src/main/java/PACKAGE/actions/CACTIONNAMEAction.java
@@ -1,14 +1,17 @@
-package {{{ package }}}.actions
+package {{{ package }}}.actions;
 
 import org.lightjason.agentspeak.action.IBaseAction;
 import org.lightjason.agentspeak.common.CPath;
 import org.lightjason.agentspeak.common.IPath;
+import org.lightjason.agentspeak.language.CCommon;
+import org.lightjason.agentspeak.language.CRawTerm;
 import org.lightjason.agentspeak.language.ITerm;
 import org.lightjason.agentspeak.language.execution.IContext;
 import org.lightjason.agentspeak.language.execution.fuzzy.CFuzzyValue;
 import org.lightjason.agentspeak.language.execution.fuzzy.IFuzzyValue;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public final class C{{{ actionname }}}Action  extends IBaseAction
@@ -30,8 +33,33 @@ public final class C{{{ actionname }}}Action  extends IBaseAction
                                                final List<ITerm> p_annotation
     )
     {
+        // unflatten all arguments for removing lists with [ [], [ [] ] ] and check unflatten arguments
+        final List<ITerm> l_arguments = CCommon.flatcollection( p_argument ).collect( Collectors.toList() );
+        // check unflatten arguments
+        if ( l_arguments.size() < {{{ actionarguments }}} )
+            return CFuzzyValue.from( false );
+
+        // get first argument e.g. as string
+        final String l_string = l_arguments.get( 0 ).<String>raw()
+
+        // iterate over all other arguments
+        l_arguments.stream()
+                   // skip first argument
+                   .skip( 1 )
+
+                   // extract term value e.g. for number
+                   .map( ITerm::<Number>raw )
+                   // convert it to double (so here can we deal with integer and double)
+                   .mapToDouble( Number::doubleValue )
+                   // create a Double object
+                   .boxed()
+
+                   // encapsulate Java type into term type
+                   .map( CRawTerm::from )
+                   // add it to return argument
+                   .forEach( p_return::add );
+
         return CFuzzyValue.from( true );
     }
     
 }
-
