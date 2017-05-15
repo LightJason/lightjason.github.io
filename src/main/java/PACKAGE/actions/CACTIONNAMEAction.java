094d57d9b49f4c03d4fc205be2713530a8b51564
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-05-15 16:10:22 +0200

    remove annotation

--- src/main/java/PACKAGE/actions/CACTIONNAMEAction.java
+++ src/main/java/PACKAGE/actions/CACTIONNAMEAction.java
@@ -31,9 +31,7 @@ public final class C{{{ actionname }}}Action  extends IBaseAction
     }
 
     @Override
-    public final IFuzzyValue<Boolean> execute( final IContext p_context, final boolean p_parallel, final List<ITerm> p_argument, final List<ITerm> p_return,
-                                               final List<ITerm> p_annotation
-    )
+    public final IFuzzyValue<Boolean> execute( final IContext p_context, final boolean p_parallel, final List<ITerm> p_argument, final List<ITerm> p_return )
     {
         // unflatten all arguments for removing lists with [ [], [ [] ] ] and check unflatten arguments
         final List<ITerm> l_arguments = CCommon.flatcollection( p_argument ).collect( Collectors.toList() );
