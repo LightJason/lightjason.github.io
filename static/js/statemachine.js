e53a7741e655ac88dbab13127a706032de2849fb
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-06-25 23:08:45 +0200

    add animation

--- static/js/statemachine.js
+++ static/js/statemachine.js
@@ -2,11 +2,48 @@
 
 jQuery(function() {
 
+    var ll_swap = true;
+    var ln_index = 0;
+    var la_validorder = ["state-1", "path-1to1", "path-1to1", "path-1to1", "path-1to1", "path-1to1", "path-1to1", "path-1to2", "path-2to2", "path-2to2", "path-2to3", "path-2to3"];
+
+
+    var lo_colorset = function(i, p_prefix, p_colorfill, p_colorline) {
+        if (i.startsWith("state"))
+            jQuery( p_prefix+i ).attr("fill", p_colorfill);
+        if (i.startsWith("path"))
+            jQuery( p_prefix+i ).attr("stroke", p_colorline);        
+    };
+
+    var lo_valid = function() {
+        if (ll_swap)
+        {
+            lo_colorset( la_validorder[ln_index], "#", "#f90", "#f90" );
+            ln_index++;
+            ll_swap = false;
+        }
+        else
+        {
+            lo_colorset( la_validorder[ln_index-1], "#", "#fff", "#000" );
+            ll_swap = true;
+        }
+
+        if (ln_index < la_validorder.length - 1 )
+            setTimeout( lo_valid, 1000 );
+        else
+        {
+            ln_index = 0;
+            ll_swap = true;
+        }
+    }
+
+
     // animate of string
     jQuery("#animate-valid").click( function() {
-        ["state-1", "path_1to1", "path_1to1", "path_1to1", "path_1to1", "path_1to1", "path_1to1", "path_1to2", "path_2to2", "path_2to2", "path_2to3", "path_2to3"].forEach( function(i) {
+        if ( ln_index != 0)
+            return;
 
-        });
+        setTimeout(lo_valid, 1000);
+        
     });
 
     // defines hover effects for the text linkage
