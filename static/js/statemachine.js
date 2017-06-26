e5aedb6715aa059d5b510e0fa8b2ce59f6fcc727
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-06-26 12:08:51 +0200

    execution

--- static/js/statemachine.js
+++ static/js/statemachine.js
@@ -2,48 +2,66 @@
 
 jQuery(function() {
 
-    var ll_swap = true;
-    var ln_index = 0;
-    var la_validorder = ["state-1", "path-1to1", "path-1to1", "path-1to1", "path-1to1", "path-1to1", "path-1to1", "path-1to2", "path-2to2", "path-2to2", "path-2to3", "path-2to3"];
-
-
-    var lo_colorset = function(i, p_prefix, p_colorfill, p_colorline) {
-        if (i.startsWith("state"))
-            jQuery( p_prefix+i ).attr("fill", p_colorfill);
-        if (i.startsWith("path"))
-            jQuery( p_prefix+i ).attr("stroke", p_colorline);        
-    };
-
-    var lo_valid = function() {
-        if (ll_swap)
+    // state-machine execution algorithm (based on dom ids)
+    var lx_statemachineexecution = function( po_model, pn_time, pc_activecolor, pc_colorfillreset, pc_colorlinereset )
+    {
+        // if wait-state
+        if ( po_model.wait )
         {
-            lo_colorset( la_validorder[ln_index], "#", "#f90", "#f90" );
-            ln_index++;
-            ll_swap = false;
+            po_model.wait = !po_model.wait;
+            po_model.stateactive = !po_model.stateactive;
+
+            if ( po_model.stateactive )
+            {
+                jQuery( "#" + po_model.state[po_model.index] ).attr( "fill", pc_activecolor );
+                jQuery( "#" + po_model.transition[po_model.index] ).attr( "stroke", pc_colorlinereset );
+            }
+            else
+            { 
+                jQuery( "#" + po_model.state[po_model.index] ).attr( "fill", pc_colorfillreset );
+                jQuery( "#" + po_model.transition[po_model.index] ).attr( "stroke", pc_activecolor );
+            }
+
+            po_model.index++;
+            po_model.timeout = setTimeout( function() { lx_statemachineexecution( po_model, pn_time, pc_activecolor, pc_colorfillreset, pc_colorlinereset ) }, pn_time );
+            return;
+        }
+
+        if ( po_model.stateactive )
+        {
+            jQuery( "#" + po_model.state[po_model.index] ).attr( "fill", pc_activecolor );
+            jQuery( "#" + po_model.transition[po_model.index] ).attr( "stroke", pc_colorlinereset );
         }
         else
-        {
-            lo_colorset( la_validorder[ln_index-1], "#", "#fff", "#000" );
-            ll_swap = true;
-        }
+        { 
+            jQuery( "#" + po_model.state[po_model.index] ).attr( "fill", pc_colorfillreset );
+            jQuery( "#" + po_model.transition[po_model.index] ).attr( "stroke", pc_activecolor );
+        }   
 
-        if (ln_index < la_validorder.length - 1 )
-            setTimeout( lo_valid, 1000 );
+        po_model.wait = !po_model.wait;    
+        if ( po_model.index < po_model.state.length )
+            po_model.timeout = setTimeout( function() { lx_statemachineexecution( po_model, pn_time, pc_activecolor, pc_colorfillreset, pc_colorlinereset ) }, pn_time ); 
         else
-        {
-            ln_index = 0;
-            ll_swap = true;
-        }
+            clearTimeout( po_model.timeout );
+    }    
+
+    // valid finite-state execution
+    var lo_valid = {
+        index       : 0,
+        wait        : false,
+        timeout     : 0,  
+        stateactive : true,
+        string      : "aaabbb169XxX",
+        transition  : [ "path-1to1", "path-1to1", "path-1to1", "path-1to1", "path-1to1", "path-1to1", "path-1to2", "path-2to2", "path-2to2", "path-2to3", "path-3to4" ],
+        state       : [ "state-1",   "state-1",   "state-1",   "state-1",   "state-1",   "state-1",   "state-1",   "state-2",   "state-2",   "state-2",   "state-3",   "state-4" ] 
     }
-
 
     // animate of string
     jQuery("#animate-valid").click( function() {
-        if ( ln_index != 0)
+        if ( lo_valid.timout )
             return;
 
-        setTimeout(lo_valid, 1000);
-        
+        lx_statemachineexecution( lo_valid, 1000, "#f90", "#fff", "#000" );
     });
 
     // defines hover effects for the text linkage
