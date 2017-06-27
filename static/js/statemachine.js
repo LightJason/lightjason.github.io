2c910b1f8d5d35ab00d6cb4c5053f9fe06222fc1
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-06-27 15:56:04 +0200

    add layout

--- static/js/statemachine.js
+++ static/js/statemachine.js
@@ -2,18 +2,21 @@
 
 jQuery(function() {
 
-    // valid finite-state execution
+    // valid string
     var lo_valid = {
-        string      : "aaabbb169XxX",
-        callbackStart: function(i) { jQuery( i.prefix + i.execution[0][i.execution[0].length - 1 ] ).attr("fill", i.statecolor); },
-        execution   : [["state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to2", "state-2", "path-2to2", "state-2", "path-2to2", "state-2", "path-2to3", "state-3", "path-3to4", "state-4" ]]
+        string        : "aaabbb169XxX",
+        callbackStart : function(i) { ["state-error", "state-4"].forEach(function(j) { jQuery( i.prefix + j ).attr("fill", i.statecolor); }); },
+        execution     : [["state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to2", "state-2", "path-2to2", "state-2", "path-2to2", "state-2", "path-2to3", "state-3", "path-3to4", "state-4" ]]
     };
 
     // non-valid finite-state execution
     var lo_nonvalid = {
+        string        : "AA2b",
+        callbackStart : function(i) { ["state-error", "state-4"].forEach(function(j) { jQuery( i.prefix + j ).attr("fill", i.statecolor); }); },
+        execution     : [["state-1", "path-1to1", "state-1", "path-1to1", "path-1to2", "state-2", "path-2toerror", "state-error"]]
     };
 
-    // animate of string
+    // animates of strings
     jQuery("#animate-valid").click( function() {
         if ( lo_valid.timout )
             return;
@@ -21,10 +24,18 @@ jQuery(function() {
         lo_valid = LJason.finitestatemachine( lo_valid );
     });
 
+    jQuery("#animate-nonvalid").click( function() {
+        if ( lo_nonvalid.timout )
+            return;
+
+        lo_nonvalid = LJason.finitestatemachine( lo_nonvalid );
+    });
+
+
     // defines hover effects for the text linkage
 	[ "state-1", "state-4", "state-error" ].forEach( function(i) {
 		jQuery("#animate-" + i).hover( 
-			function() { jQuery("#" + i).attr("fill", "#f90"); },
+			function() { jQuery("#" + i).attr("fill", "#0e7"); },
 			function() { jQuery("#" + i).attr("fill", "#fff"); } 
 		);
 	});    
