7b6b775b7ea5fb09c9d30914c494afeb5b2d1644
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-06-27 17:58:28 +0200

    execution function

--- static/js/statemachine.js
+++ static/js/statemachine.js
@@ -2,35 +2,54 @@
 
 jQuery(function() {
 
-    // state-reset function
-    var lx_resetcallback = function(i) { ["state-error", "state-4"].forEach(function(j) { jQuery( i.prefix + j ).attr("fill", i.statecolor); }); };
+    // state-machine reset function
+    var lx_resetcallback = function(i) {
+        var la_unchecked = i.dom.parent().find(".unchecked");
+        i.dom.parent().find(".checked").each(function(n,m) { var lo = jQuery(m); jQuery(la_unchecked[n]).text( lo.text() ); lo.empty(); });
 
-    // valid string
+        ["state-error", "state-4"].forEach(function(j) { jQuery( i.prefix + j ).attr("fill", i.statecolor); }); 
+    };
+
+    // state-machine copy char
+    var lx_stringcopy = function(i) {
+        if ( !i.execution[0][i.index].startsWith(i.transitionstring) )
+            return;
+
+        var la_checked = i.dom.parent().find(".checked");
+        i.dom.parent().find(".unchecked").each(function(n,m) { var lo_checked = jQuery(la_checked[n]); var lo_unchecked = jQuery(m); lo_checked.text( lo_checked.text() + lo_unchecked.text().charAt(0) ); lo_unchecked.text( lo_unchecked.text().substring(1) ); });
+    };
+
+    // valid-string state-machine execution
     var lo_valid = {
-        string        : "aaabbb169XxX",
         callbackStart : lx_resetcallback,
+        callbackNext  : lx_stringcopy,
         execution     : [["state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to1", "state-1", "path-1to2", "state-2", "path-2to2", "state-2", "path-2to2", "state-2", "path-2to3", "state-3", "path-3to4", "state-4" ]]
     };
 
-    // non-valid finite-state execution
+    // non-valid state-machine execution
     var lo_nonvalid = {
-        string        : "AA2b",
         callbackStart : lx_resetcallback,
+        callbackNext  : lx_stringcopy,
         execution     : [["state-1", "path-1to1", "state-1", "path-1to1", "path-1to2", "state-2", "path-2toerror", "state-error"]]
     };
 
-    // animates of strings
+
+    // animate state-machine
+    jQuery(".unchecked").css( "opacity", 0.3 );
+
     jQuery("#animate-valid").click( function() {
-        if ( lo_valid.timout )
+        if ( ( lo_nonvalid.timout ) || ( lo_valid.timout ) )
             return;
 
+        lo_valid.dom = jQuery(this);
         lo_valid = LJason.finitestatemachine( lo_valid );
     });
 
     jQuery("#animate-nonvalid").click( function() {
-        if ( lo_nonvalid.timout )
+        if ( ( lo_nonvalid.timout ) || ( lo_valid.timout ) )
             return;
 
+        lo_nonvalid.dom = jQuery(this);
         lo_nonvalid = LJason.finitestatemachine( lo_nonvalid );
     });
 
